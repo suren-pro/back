d4ddcb760d55e8aedc75570a4d7ff7d935c85f10
Suren <surenbarseghyan13@gmail.com>
2020-12-01 16:19:12 +0400

    Add graph

--- Db Manager/StatisticsManager.cs
+++ Db Manager/StatisticsManager.cs
@@ -371,8 +371,103 @@ namespace HouseholdUserApplication.Db_Manager
             }
             return utilityDetails;
             }
+    public static Total GetTotalGraphByYear(Dates dates,int userId)
+    {
+        Total total = new Total();
+        total.Utilities = new List<List<object>>();
+        List<string> names = new List<string>();
+        List<UtilityRecord> utilityRecords = new List<UtilityRecord>();
+        List<UtilityDetail> utilityDetails = new List<UtilityDetail>();
+        using (MySqlConnection conn = new MySqlConnection(ConnectionString.Build()))
+        {
+            conn.Open();
+            using (MySqlCommand cmd = new MySqlCommand())
+            {
+                cmd.Connection = conn;
+                cmd.CommandText = @"SELECT name FROM utilities";
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        total.Utilities.Add(new List<object>() { reader["name"].ToString() });
+                    }
+                }
+            }
+            using (MySqlCommand cmd = new MySqlCommand())
+            {
+                cmd.Connection = conn;
+                cmd.CommandText = @"SELECT utility_record_id,date,value,U.address,name,price
+                                        FROM utility_records AS UR 
+                                        INNER JOIN residents_addresses AS U
+                                         ON UR.address = U.address
+                                        INNER JOIN utilities 
+                                        ON utility = utility_id
+                                        WHERE U.resident = @userId
+                                        AND year(date) = @year
+                                         ORDER BY utility,date";
+                cmd.Parameters.AddWithValue("@userId", (Convert.ToInt32(userId)));
+                cmd.Parameters.AddWithValue("@year", (Convert.ToInt32(dates.Years[0])));
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        UtilityRecord utilityRecord = new UtilityRecord
+                        {
+                            Value = (int)reader["value"],
+                            Date = Convert.ToDateTime(reader["date"])
+                        };
+                        Utility utility = new Utility
+                        {
+                            Name = reader["name"].ToString(),
+                            Price = (double)reader["price"],
+
+                        };
+                        utilityRecord.Utility = utility;
+                        utilityRecords.Add(utilityRecord);
+                    }
+                }
+            }
+            int next = 0, current = 0;
+
+            for (int i = 0; i < utilityRecords.Count - 1; i++)
+            {
+
+
+                if (utilityRecords[i].Utility.Name == utilityRecords[i + 1].Utility.Name)
+                {
+                    double price = utilityRecords[i].Utility.Price;
+                    current = utilityRecords[i].Value;
+                    next = utilityRecords[i + 1].Value;
+                    UtilityDetail utilityDetail = new UtilityDetail
+                    {
+                        Name = utilityRecords[i].Utility.Name,
+                        Price = Convert.ToDouble((next - current) * price),
+                        Date = utilityRecords[i].Date
+                    };
+                    utilityDetails.Add(utilityDetail);
+
+                }
+            }
+            foreach (List<object> utilities in total.Utilities)
+            {
+                foreach (var utilityDetail in utilityDetails)
+                {
+                    if (utilityDetail.Name == utilities[0].ToString())
+                        utilities.Add(utilityDetail.Price);
+                }
+            }
+
+        }
+        total.Services = new Dictionary<string, int>
+            {
+                {"Nanny",500 },
+                { "Cleaning",600}
+            };
+        return total;
+    }
         }
 
 
     }
+
 
