54964533ca182c43109f31251b22e5a51ceee97a
Suren <surenbarseghyan13@gmail.com>
2020-12-01 11:43:35 +0400

    Refresh

--- AppSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HouseholdUserApplication
-{
-    internal class AppSettings
-    {
-        public static string Key { get; set; }
-        public static string UserName { get; set; }
-        public static string Password { get; set; }
-    }
-}
